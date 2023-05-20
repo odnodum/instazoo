f20c59ccec837cef005f7a3d277a664748f089bd
Ramil Khanov <sa-msk@yandex.ru>
2023-05-20 16:54:34 +0300

    Initial commit

---
+++ src/test/java/ru/proxyva/instazoo/InstazooApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.proxyva.instazoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstazooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
