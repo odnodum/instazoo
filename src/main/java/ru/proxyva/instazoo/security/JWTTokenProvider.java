f1c536590291afdf10ffcf7873e0e4e473cfe4e5
Ramil Khanov <sa-msk@yandex.ru>
2023-05-21 11:11:51 +0300

    JWTAuthenticationFIlter class

---
+++ src/main/java/ru/proxyva/instazoo/security/JWTTokenProvider.java
@@ -0,0 +1,67 @@
+package ru.proxyva.instazoo.security;
+
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import ru.proxyva.instazoo.entity.User;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Component
+public class JWTTokenProvider {
+
+    public static final Logger LOG = LoggerFactory.getLogger(JWTTokenProvider.class);
+
+    public String generateToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+        Date expiryDate = new Date(now.getYear() + SecurityConstants.EXPIRATION_TIME);
+
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> claimsMap = new HashMap<>();
+        claimsMap.put("id", userId);
+        claimsMap.put("username", user.getEmail());
+        claimsMap.put("firsname", user.getName());
+        claimsMap.put("lastname", user.getLastname());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .addClaims(claimsMap)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.ES512, SecurityConstants.SECRET)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(SecurityConstants.SECRET)
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SignatureException |
+                 MalformedJwtException |
+                 ExpiredJwtException |
+                 UnsupportedJwtException |
+                 IllegalArgumentException ex) {
+            LOG.error(ex.getMessage());
+            return false;
+        }
+    }
+
+    public Long getUserIdFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+        String id = (String) claims.get("id");
+        return Long.parseLong(id);
+    }
+
+}
